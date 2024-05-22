784f2c4ff4026ba9a8a8f37bb3b787536cf87d9b
isuru20ip <isuru20.ip@gmail.com>
2024-05-22 11:25:01 +0530

    c

--- advancedSearchprocess.php
+++ advancedSearchprocess.php
@@ -8,15 +8,15 @@ $to = $_POST["pt"];
 $sort = $_POST["s"];
 
 if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 0) {
-    
+
     $query = "SELECT * FROM `product`";
     $status = 0;
-    
+
     if (!empty($text)) {
         $query .= " WHERE `title` LIKE '%" . $text . "%'";
         $status = 1;
     }
-    
+
     if ($category != 0) {
         if ($status == 0) {
             $query .= " WHERE `category_id` = '" . $category . "'";
@@ -25,7 +25,7 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
             $query .= " AND `category_id` = '" . $category . "'";
         }
     }
-    
+
     if (!empty($from) && empty($to)) {
         if ($status == 0) {
             $query .= " WHERE `price` >= '" . $from . "'";
@@ -34,7 +34,7 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
             $query .= " AND  `price` >= '" . $from . "'";
         }
     }
-    
+
     if (empty($from) && !empty($to)) {
         if ($status == 0) {
             $query .= " WHERE `price` <= '" . $to . "'";
@@ -43,7 +43,7 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
             $query .= " AND  `price` <= '" . $to . "'";
         }
     }
-    
+
     if (!empty($from) && !empty($to)) {
         if ($status == 0) {
             $query .= " WHERE `price` <= '" . $to . "' AND `price` >= '" . $from . "'";
@@ -52,110 +52,110 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
             $query .= " AND `price` <= '" . $to . "' AND `price` >= '" . $from . "'";
         }
     }
-    
+
     if ($sort == 1) {
         $query .= " ORDER BY `price` DESC";
     }
-    
+
     if ($sort == 2) {
         $query .= " ORDER BY `price` ASC";
     }
-    
+
     if ($sort == 3) {
         $query .= " ORDER BY `qty` ASC";
     }
-    
+
     if ($sort == 4) {
         $query .= " ORDER BY `qty` DESC";
     }
-    
-    ?>
-    
+
+?>
+
     <?php
-    
-    
-    
+
+
+
     // ---------------------------------------------------
     ?>
     <!-- products -->
     <div class="col-12 mb-3 p-3">
         <div class="row ">
-    
+
             <div class="col-12" id="view_area">
                 <div class="row justify-content-center gap-2">
-    
+
                     <?php
-    
+
                     if ("0" !== $_POST["page"]) {
                         $pageno = $_POST["page"];
                     } else {
                         $pageno = 1;
                     }
-    
+
                     $product_rs = Database::search($query);
                     $product_num = $product_rs->num_rows;
-    
+
                     $results_per_page = 6;
                     $number_of_page = ceil($product_num / $results_per_page);
-    
+
                     $page_rs = ($pageno - 1) * $results_per_page;
                     $select_rs = Database::search($query . " LIMIT " . $results_per_page . " OFFSET " . $page_rs . " ");
-    
+
                     $select_num = $select_rs->num_rows;
-    
+
                     for ($i = 0; $i < $select_num; $i++) {
                         $product_data = $select_rs->fetch_assoc();
-    
+
                         $img_rs = Database::search("SELECT * FROM `p_img` WHERE `product_id` = '" . $product_data["id"] . "'");
                         $img_data = $img_rs->fetch_assoc();
-    
+
                     ?>
-    
+
                         <!-- card -->
                         <!-- <spin> -->
                         <div class=" col-12 col-lg-2 mt-2 mb-2 border border-1 shadow-lg bg-body-tertiary rounded" style="width: 18rem;">
-    
+
                             <img src="<?php echo $img_data["p_path"] ?>" class="card-img-top img-thumbnail mt-2 border-0" style="height: 300px;" />
-    
+
                             <div class="card-body ms-0 m-0 ">
-    
+
                                 <div class=" col-12 text-center mt-3">
                                     <a href="#" class=" col-5 btn btn-outline-danger border-3 fw-bold">Add</a>
                                     <a href="<?php echo "singleProductView.php?id=" . ($product_data["id"]); ?>" class="col-5 btn btn-outline-success border-3 fw-bold">Viwe</a>
                                 </div>
-    
+
                                 <div class=" col-12 text-center mt-3">
                                     <span class=" fw-bold text-decoration-none text-dark p-1 "> <?php echo $product_data["title"]; ?> </span>
                                 </div>
-    
+
                                 <div class=" col-12 text-center">
                                     <span class="card-text text-danger fw-bold">Rs.<?php echo $product_data["price"]; ?>.00</span><br />
-    
+
                                     <button class="col-10 btn btn-outline-light mt-3 border border-2 border-warning mb-4">
                                         <img src="resources/wish.svg" />
                                     </button>
                                 </div>
-    
+
                             </div>
                         </div>
                         <!-- <spin> -->
                         <!-- card -->
-    
+
                     <?php
                     }
                     ?>
-    
+
                 </div>
-    
+
             </div>
-    
+
         </div>
     </div>
     <!-- products -->
-    
+
     </div>
     </div>
-    
+
     <div class="offset-2 offset-lg-3 col-8 col-lg-6 text-center mb-3">
         <nav aria-label="Page navigation example">
             <ul class="pagination pagination-lg justify-content-center">
@@ -169,7 +169,7 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
                     </a>
                 </li>
                 <?php
-    
+
                 for ($x = 1; $x <= $number_of_page; $x++) {
                     if ($x == $pageno) {
                 ?>
@@ -185,9 +185,9 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
                 <?php
                     }
                 }
-    
+
                 ?>
-    
+
                 <li class="page-item">
                     <a class="page-link" <?php if ($pageno >= $number_of_page) {
                                                 echo ("#");
@@ -201,10 +201,10 @@ if (!empty($text) || $category != 0 || !empty($from) || !empty($to) || $sort != 
         </nav>
     </div>
 
-    <?php
+<?php
 
-}else{
-    echo("doNothing");
+} else {
+    echo ("doNothing");
 }
 
 ?>
