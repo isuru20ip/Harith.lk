2ec156498d91473e935fece6f5cdd678cb361de9
isuru20ip <isuru20.ip@gmail.com>
2024-05-26 11:45:59 +0530

    userp

--- updateProfileProcess.php
+++ updateProfileProcess.php
@@ -59,65 +59,70 @@ if (isset($_SESSION["user"])) {
         echo ("Postal Code must have less than 6 Charactres.");
     } else {
 
-        // <--Update Address-->
-        $adders_rs = Database::search("SELECT * FROM `address` WHERE `user_email` = '" . $email . "';");
-        $adders_num = $adders_rs->num_rows;
-
-        if ($adders_num == '1') {
-            Database::iud("UPDATE `address` SET `line_1` = '" . $line1 . "', `line_2` = '" . $line2 . "', `postal_code` = '" . $pcode . "', `city_id` = '" . $city . "' WHERE `user_email` = '" . $email . "' ");
-        } else {
-            Database::iud("INSERT INTO `address` (`line_1`,`line_2`,`postal_code`,`city_id`,`user_email`) VALUE ('" . $line1 . "','" . $line2 . "','" . $pcode . "','" . $city . "','" . $email . "')");
-        }
-        
-        //    <-- Update Img -->
-        $allowed_image_extentions = array("image/jpg", "image/jpeg", "image/png", "image/svg+xml");
-
-        if (isset($_FILES["pm"])) {
-
-            $img = $_FILES["pm"]; // image
-            $fil_type = $img["type"]; // image type
-
-            if (in_array($fil_type, $allowed_image_extentions)) {
-
-                $new_file_type;
-
-                if ($fil_type == "image/jpg") {
-                    $new_file_type = ".jpg";
-                } elseif ($fil_type == "image/jpeg") {
-                    $new_file_type = ".jpeg";
-                } elseif ($fil_type == "image/png") {
-                    $new_file_type == ".png";
-                } elseif ($fil_type == "image/svg+xml") {
-                    $new_file_type == ".svg";
-                }
-
-                $file_name = "resources//user_img//" . $lname . "_" . $phone . "_" . uniqid() . $new_file_type;
-                move_uploaded_file($img["tmp_name"], $file_name);
+        try {
 
 
-                $image_rs = Database::search("SELECT * FROM `user_img` WHERE `user_email`='" . $email . "'");
-                $image_num = $image_rs->num_rows;
+            // <--Update Address-->
+            $adders_rs = Database::search("SELECT * FROM `address` WHERE `user_email` = '" . $email . "';");
+            $adders_num = $adders_rs->num_rows;
 
-                if ($image_num == 1) {
-                    Database::iud("UPDATE `user_img` SET `path`='" . $file_name . "' WHERE `user_email`='" . $email . "'");
-                } else {
-                    Database::iud("INSERT INTO `user_img` (`path`,`user_email`) VALUES('" . $file_name . "' ,'" . $email . "')");
-                }
-
+            if ($adders_num == '1') {
+                Database::iud("UPDATE `address` SET `line_1` = '" . $line1 . "', `line_2` = '" . $line2 . "', `postal_code` = '" . $pcode . "', `city_id` = '" . $city . "' WHERE `user_email` = '" . $email . "' ");
             } else {
-                echo ("Fill type does not allowed to uplord");
+                Database::iud("INSERT INTO `address` (`line_1`,`line_2`,`postal_code`,`city_id`,`user_email`) VALUE ('" . $line1 . "','" . $line2 . "','" . $pcode . "','" . $city . "','" . $email . "')");
             }
-        }
 
-        //    <--Update User-->
-        $user_rs = Database::search("SELECT * FROM `user` WHERE `email` = '".$email."'");
-        $user_num = $user_rs->num_rows;
+            //    <-- Update Img -->
+            $allowed_image_extentions = array("image/jpg", "image/jpeg", "image/png", "image/svg+xml");
 
-        if ($user_num == 1) {
-            Database::iud("UPDATE `user` SET `fname` = '".$fname."', `lname` = '".$lname."', `password` = '".$pw."', `contact_no`='".$phone."' WHERE `email` = '".$email."'");
-            echo("Done");
-        }else{
-            echo ("You are not valid user ");
+            if (isset($_FILES["pm"])) {
+
+                $img = $_FILES["pm"]; // image
+                $fil_type = $img["type"]; // image type
+
+                if (in_array($fil_type, $allowed_image_extentions)) {
+
+                    $new_file_type;
+
+                    if ($fil_type == "image/jpg") {
+                        $new_file_type = ".jpg";
+                    } elseif ($fil_type == "image/jpeg") {
+                        $new_file_type = ".jpeg";
+                    } elseif ($fil_type == "image/png") {
+                        $new_file_type == ".png";
+                    } elseif ($fil_type == "image/svg+xml") {
+                        $new_file_type == ".svg";
+                    }
+
+                    $file_name = "resources//user_img//" . $lname . "_" . $phone . "_" . uniqid() . $new_file_type;
+                    move_uploaded_file($img["tmp_name"], $file_name);
+
+
+                    $image_rs = Database::search("SELECT * FROM `user_img` WHERE `user_email`='" . $email . "'");
+                    $image_num = $image_rs->num_rows;
+
+                    if ($image_num == 1) {
+                        Database::iud("UPDATE `user_img` SET `path`='" . $file_name . "' WHERE `user_email`='" . $email . "'");
+                    } else {
+                        Database::iud("INSERT INTO `user_img` (`path`,`user_email`) VALUES('" . $file_name . "' ,'" . $email . "')");
+                    }
+                } else {
+                    echo ("Fill type does not allowed to uplord");
+                }
+            }
+
+            //    <--Update User-->
+            $user_rs = Database::search("SELECT * FROM `user` WHERE `email` = '" . $email . "'");
+            $user_num = $user_rs->num_rows;
+
+            if ($user_num == 1) {
+                Database::iud("UPDATE `user` SET `fname` = '" . $fname . "', `lname` = '" . $lname . "', `password` = '" . $pw . "', `contact_no`='" . $phone . "' WHERE `email` = '" . $email . "'");
+                echo ("Done");
+            } else {
+                echo ("You are not valid user ");
+            }
+        } catch (\Throwable $th) {
+            echo('Some thing went wrong Please Try again');
         }
     }
 }
