01072a732dbe62ceaf80b1c0c65c3d0ad92d866f
isuru20ip <isuru20.ip@gmail.com>
2024-05-28 10:21:07 +0530

    feedtry

--- feedbackprocess.php
+++ feedbackprocess.php
@@ -22,39 +22,43 @@ if (isset($_SESSION["user"])) {
         $d->setTimezone($tz);
         $date = $d->format("Y-m-d H:i:s");
 
-        Database::search("INSERT INTO `feedback` (`text`,`user_email`,`product_id`,`date`) VALUES ('" . $text . "','" . $email . "','" . $pid . "','" . $date . "')");
+        try {
+            Database::search("INSERT INTO `feedback` (`text`,`user_email`,`product_id`,`date`) VALUES ('" . $text . "','" . $email . "','" . $pid . "','" . $date . "')");
 
-        $fee_id = Database::$connection->insert_id;
-
-        //    <-- Update Img -->
-        $allowed_image_extentions = array("image/jpg", "image/jpeg", "image/png", "image/svg+xml");
-
-        if (isset($_FILES["pimg"])) {
-
-            $img = $_FILES["pimg"];
-            $fil_type = $img["type"];
-
-            if (in_array($fil_type, $allowed_image_extentions)) {
-
-                $new_file_type;
-
-                if ($fil_type == "image/jpg") {
-                    $new_file_type = ".jpg";
-                } elseif ($fil_type == "image/jpeg") {
-                    $new_file_type = ".jpeg";
-                } elseif ($fil_type == "image/png") {
-                    $new_file_type == ".png";
-                } elseif ($fil_type == "image/svg+xml") {
-                    $new_file_type == ".svg";
+            $fee_id = Database::$connection->insert_id;
+    
+            //    <-- Update Img -->
+            $allowed_image_extentions = array("image/jpg", "image/jpeg", "image/png", "image/svg+xml");
+    
+            if (isset($_FILES["pimg"])) {
+    
+                $img = $_FILES["pimg"];
+                $fil_type = $img["type"];
+    
+                if (in_array($fil_type, $allowed_image_extentions)) {
+    
+                    $new_file_type;
+    
+                    if ($fil_type == "image/jpg") {
+                        $new_file_type = ".jpg";
+                    } elseif ($fil_type == "image/jpeg") {
+                        $new_file_type = ".jpeg";
+                    } elseif ($fil_type == "image/png") {
+                        $new_file_type == ".png";
+                    } elseif ($fil_type == "image/svg+xml") {
+                        $new_file_type == ".svg";
+                    }
+                    $file_name = "resources//user_img//" . uniqid() .$new_file_type;
+                    move_uploaded_file($img["tmp_name"], $file_name);
+    
+                    Database::iud("INSERT INTO `feed_img` (`path`,`feedback_id`) VALUES ('" . $file_name . "','" . $fee_id . "')");
+                } else {
+                    echo ("Fill type does not allowed to uplord");
                 }
-                $file_name = "resources//user_img//" . uniqid() .$new_file_type;
-                move_uploaded_file($img["tmp_name"], $file_name);
-
-                Database::iud("INSERT INTO `feed_img` (`path`,`feedback_id`) VALUES ('" . $file_name . "','" . $fee_id . "')");
-            } else {
-                echo ("Fill type does not allowed to uplord");
             }
+            echo ("OK");
+        } catch (\Throwable $th) {
+           echo("An Error occurs, try Again");
         }
-        echo ("OK");
     }
 }
