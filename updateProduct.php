5a1382ac89654693e806f4b7ddb4017d508c6dfd
isuru20ip <isuru20.ip@gmail.com>
2024-05-28 13:41:02 +0530

    updatePDesign

--- updateProduct.php
+++ updateProduct.php
@@ -22,34 +22,31 @@ if (isset($_SESSION["p"])) {
     </head>
 
     <body>
-        <div class=" container-fluid ">
+        <div class=" container-fluid bg-body-tertiary">
             <div class=" row">
+
                 <?php require "cpanal_head.php"; ?>
-                <div class=" col-12 bg-secondary-subtle ">
-                    <div class=" row">
 
-                        <div class=" col-12">
-                            <div class=" row">
+                <?php
+                $pid = $_SESSION["p"]["id"];
+                $product_rs = Database::search("SELECT * FROM `product` INNER JOIN `category` ON category.id = product.category_id WHERE product.id = '" . $pid . "'");
+                $product_data = $product_rs->fetch_assoc();
+                ?>
 
-                                <div class=" col-12 text-center">
-                                    <h3 class="fw-bold p-2 rounded-2 "> Update Product</h3>
-                                </div>
+                <div class=" col-12 col-lg-6 offset-lg-3 card my-5 shadow shadow-lg">
+                    <div class="row">
 
-                            </div>
+                        <div class=" col-12 text-center">
+                            <h3 class="fw-bold p-2 rounded-2 "> Update Product</h3>
+                            <hr />
                         </div>
 
-                        <?php
-                        $pid = $_SESSION["p"]["id"];
-                        $product_rs = Database::search("SELECT * FROM `product` INNER JOIN `category` ON category.id = product.category_id WHERE product.id = '" . $pid . "'");
-                        $product_data = $product_rs->fetch_assoc();
-                        ?>
-
-                        <div class=" col-12 col-lg-6 border border-1 border-black rounded-3 bg-white">
+                        <div class=" col-12">
                             <!-- <div class=" row"> -->
 
                             <div class=" col-12 p-3">
                                 <label class=" form-label fw-bold "> Product Name</label>
-                                <input type="text" class=" form-control border border-1 border-black" id="pname" value="<?php echo $product_data["title"]; ?>" />
+                                <input type="text" class=" form-control border border-1 border-black" id="pname" value="<?php echo $product_data["title"]; ?>" disabled />
                             </div>
 
                             <div class=" col-12 p-3">
@@ -90,76 +87,65 @@ if (isset($_SESSION["p"])) {
 
                                 </div>
                             </div>
-                        </div>
 
-                        <!-- <div class=" row"> -->
-                        <div class=" col-12 col-lg-6 border border-1 border-black rounded-3 bg-white">
-
-                            <div class="col-12 text-center bg-secondary-subtle rounded-2">
-                                <label class="form-label fw-bold fs-4">Product Description</label>
-                            </div>
-                            <div class="col-12">
-                                <textarea cols="30" rows="19" class=" form-control" id="desc"><?php echo $product_data["description"]; ?></textarea>
-                            </div>
-                        </div>
-
-                        <!-- <div class=" row"> -->
-                        <div class=" col-12 border border-1 border-black rounded-3">
-
-                            <div class="col-12 text-center">
-                                <label class="form-label fw-bold fs-4">Product Images</label>
-                            </div>
-                        </div>
-
-                        <!-- <div class=" row"> -->
-                        <div class=" col-12 border border-1 border-black rounded-3">
-
-                            <div class="col-12">
-
-                                <div class=" text-center">
-                                    <div class="row">
-
-                                        <?php
-                                        $pimg_rs = Database::search("SELECT * FROM `p_img` WHERE product_id = '" . $pid . "'");
-                                        $pimg_num = $pimg_rs->num_rows;
-                                        
-                                        for ($i = 0; $i <3; $i++) {
-                                            $pimg_data = $pimg_rs->fetch_assoc();
-                                        ?>
-                                            <div class=" col-12 col-lg-4 border border-primary rounded">
-                                                <?php
-                                                if (isset($pimg_data["p_path"])) {
-                                                ?>
-                                                    <img src="<?php echo $pimg_data["p_path"]; ?>" class="img-fluid" style="width: 250px;" id="i<?php echo $i; ?>">
-                                                <?php
-                                                } else {
-                                                ?><img src="resources/up.png" class="img-fluid" style="width: 250px;" id="i<?php echo $i; ?>"><?php
-                                                                                                                                } ?>
-                                            </div>
-                                        <?php
-                                        }
-                                        ?>
-                                        <input type="file" id="pimg" multiple hidden>
-                                    </div>
+                            <!-- <div class=" row"> -->
+                            <div class=" col-12 p-3">
+                                <label class="form-label fw-bold">Product Description</label>
+                                <div class="col-12">
+                                    <textarea cols="30" rows="19" class=" form-control" id="desc"><?php echo $product_data["description"]; ?></textarea>
                                 </div>
+                            </div>
 
+                            <!-- <div class=" row"> -->
+                            <div class=" col-12 p-3">
+                                <label class="form-label fw-bold">Product Images</label>
+
+                                <?php
+                                $pimg_rs = Database::search("SELECT * FROM `p_img` WHERE product_id = '" . $pid . "'");
+                                $pimg_num = $pimg_rs->num_rows;
+
+                                for ($i = 0; $i < 3; $i++) {
+                                    $pimg_data = $pimg_rs->fetch_assoc();
+                                ?>
+                                    <div class=" col-12 d-flex justify-content-center border">
+                                        <?php
+                                        if (isset($pimg_data["p_path"])) {
+                                        ?>
+                                            <img src="<?php echo $pimg_data["p_path"]; ?>" style="width: 250px; height : 250px;" id="i<?php echo $i; ?>">
+                                        <?php
+                                        } else {
+                                        ?><img src="resources/up.png" style="width: 250px; height : 250px;" id="i<?php echo $i; ?>"><?php
+                                                                                                                                } ?>
+                                    </div>
+                                <?php
+                                }
+                                ?>
+                                <input type="file" id="pimg" multiple hidden>
                             </div>
                         </div>
 
-                        <!-- <div class=" row"> -->
-                        <div class="col-12 text-center">
-                            <label class=" col-12 col-lg-4 form-label fs-5 fw-bold btn btn-info mt-2" for="pimg" onclick="changeProductImage();">Uploard Images</label>
+
+                        <div class="col-12 d-flex justify-content-end ">
+                            <div class="row">
+
+                                <div>
+                                    <label class="btn btn-warning fw-bold mb-2" for="pimg" onclick="changeProductImage();">Uploard Images</label>
+                                    <button class="btn btn-success fw-bold mb-2" onclick="updateproduct();"> Update Product </button>
+                                </div>
+                            </div>
                         </div>
 
-                        <div class=" col-12 mt-4">
-                            <button class=" col-12 btn btn-success mb-4 " onclick="updateproduct();"> Update Product </button>
-                        </div>
+
+
+
+
 
 
                     </div>
 
                 </div>
             </div>
+
 
         </div>
 
