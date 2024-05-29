a2162cec20b9ae88e2a709eeb1cd41f825c97882
isuru20ip <isuru20.ip@gmail.com>
2024-05-29 10:01:52 +0530

    cpanal_done

--- cpanal_head.php
+++ cpanal_head.php
@@ -1,76 +1,44 @@
-<!DOCTYPE html>
-<html>
+<div class="col-12 col-lg-4 bg-primary">
+    <div class="row">
 
-<head>
+        <div class="col-12 col-lg-4 mt-1 mb-1 text-center">
+            <?php
+            $pimg_rs = Database::search("SELECT * FROM `user_img` WHERE `user_email` = '" . $_SESSION["admin"]["user_email"] . "'");
+            $pimg_num = $pimg_rs->num_rows;
+            $pimg_data = $pimg_rs->fetch_assoc();
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Contral Panal | AYUNA.lk | Enriching Age</title>
-
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="bootstrap.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="icon" href="resources/favicon.svg">
-</head>
-
-<body>
-
-    <div class=" col-12 bg-primary">
-        <div class=" row">
-
-            <div class="col-12 col-lg-4">
-                <div class="row">
-
-                    <div class="col-12 col-lg-4 mt-1 mb-1 text-center">
-                        <?php
-                        $pimg_rs = Database::search("SELECT * FROM `user_img` WHERE `user_email` = '" . $_SESSION["admin"]["user_email"] . "'");
-                        $pimg_num = $pimg_rs->num_rows;
-                        $pimg_data = $pimg_rs->fetch_assoc();
-
-                        if ($pimg_num == '1') {
-                        ?>
-                            <img src="<?php echo $pimg_data["path"]; ?>" width="90px" height="90px" class="rounded-circle" />
-                        <?php
-                        } else {
-                        ?>
-                            <img src="resources/user.svg" width="90px" height="90px" class="rounded-circle" />
-                        <?php
-                        }
-                        ?>
-                    </div>
-
-                    <div class="col-12 col-lg-8">
-                        <div class="row text-center text-lg-start">
-                            <div class="col-12 mt-0 mt-lg-4">
-                                <span class="text-white fw-bold"><?php echo $_SESSION["admin"]["admin_name"]; ?></span>
-                            </div>
-                            <div class="col-12">
-                                <span class="text-black-50 fw-bold"><?php echo $_SESSION["admin"]["user_email"] ?></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-12 col-lg-8">
-                <div class="row">
-                    <div class="col-12 col-lg-10 mt-2 my-lg-4">
-                        <h1 class="offset-sm-3 offset-lg-1 text-white fw-bold">Admin control panel</h1>
-                    </div>
-                    <div class="col-12 col-lg-2 mx-2 mb-2 my-lg-4 mx-lg-0 d-grid">
-                        <a class="btn btn-dark fw-bold" href="control_panel.php">Contral Panal</a>
-                    </div>
-                </div>
-            </div>
-
+            if ($pimg_num == '1') {
+            ?>
+                <img src="<?php echo $pimg_data["path"]; ?>" width="90px" height="90px" class="rounded-circle" />
+            <?php
+            } else {
+            ?>
+                <img src="resources/user.svg" width="90px" height="90px" class="rounded-circle" />
+            <?php
+            }
+            ?>
         </div>
 
+        <div class="col-12 col-lg-6">
+            <div class="row text-center text-lg-start">
+                <div class="col-12 mt-0 mt-lg-4">
+                    <span class="text-white fw-bold"><?php echo $_SESSION["admin"]["admin_name"]; ?></span>
+                </div>
+                <div class="col-12">
+                    <span class="text-black-50 fw-bold"><?php echo $_SESSION["admin"]["user_email"] ?></span>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 
-
-
-    <script src="script.js"></script>
-</body>
-
-</html>
+<div class="col-12 col-lg-8 bg-primary">
+    <div class="row">
+        <div class="col-12 col-lg-10 mt-2 my-lg-4">
+            <h1 class="offset-sm-3 offset-lg-1 text-white fw-bold">Admin control panel</h1>
+        </div>
+        <div class="col-12 col-lg-2 mx-2 mb-2 my-lg-4 mx-lg-0 d-grid">
+            <a class="btn btn-dark fw-bold" href="control_panel.php">Contral Panal</a>
+        </div>
+    </div>
+</div>
